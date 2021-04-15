4e40266e623219bee0fa13c5cd64a33b660d02ae
Kervens Jasmin <jakejasminjk@gmail.com>
2021-04-14 22:50:29 -0400

    sort and filter

--- pages/createPost.js
+++ pages/createPost.js
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import fetch from 'isomorphic-unfetch';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import { fromJSON } from 'postcss';
 
 
 
@@ -19,7 +20,8 @@ import Link from 'next/link';
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [submitted, setSubmitted] = useState(false);
     const router = useRouter();
-
+    const allTags = ["events", "activites","questions","meta","rant"]
+    let userTags = [];
     useEffect((() => {
         setUser(localStorage.getItem("Current_Name"))
     }),[]);
@@ -49,12 +51,21 @@ import Link from 'next/link';
         setForm({
             ...form,
             author: userName,
+            tags: userTags,
             [e.target.name]: e.target.value
         })
     }
 
 
-
+    const addTag = (e) => {
+        var checkbox = document.getElementById(e.target.name);
+    if (checkbox.checked != false) {
+	    //userTags.push(e.target.name)
+        form.tags.push(e.target.name)
+    }else{
+	    form.tags = form.tags.filter(tag => tag!=e.target.name);
+    }
+    }
 
 
 
@@ -69,6 +80,31 @@ import Link from 'next/link';
                     <input className="block px-1 mt-2 w-full text-gray-500 appearance-none border-b-2 border-gray-100 focus:border-gray-400 hover:border-gray-800 " onChange={handleChange} type="text" id="title" name="title" placeholder="Title" /><br/>
                     <label className="text-sm" htmlFor="content">Content</label><br/>
                     <input className="block px-1 mt-2 w-full text-gray-500 appearance-none border-b-2 border-gray-100 focus:border-gray-400 hover:border-gray-800" onChange={handleChange} type="text" id="content" name="content" placeholder="Content" /><br/>
+
+                    <fieldset>
+                        <legend>Choose your interests</legend>
+                        <div>
+                            <input type="checkbox" id="events" name="events" value="events" onClick={addTag}/>
+                            <label for="events"> Events</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="activites" name="activites" value="activites" onClick={addTag}/>
+                            <label for="activites"> Activites</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="questions" name="questions" value="questions" onClick={addTag}/>
+                            <label for="questions"> Questions</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="meta" name="meta" value="meta" onClick={addTag}/>
+                            <label for="activites"> Meta</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" id="rant" name="rant" value="rant" onClick={addTag}/>
+                            <label for="activites"> Rant</label>
+                        </div>
+                        
+                    </fieldset>
                     <button className="m-auto w-full bg-red-800 rounded-md text-white mt-3 mb-2 h-10" type="submit">Post</button>
       </form> 
         
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,82 @@ import Head from 'next/head';
 import Link from 'next/link';
 import HomeItem from '../components/HomeItem'
 import fetch from 'isomorphic-unfetch';
+import { useState, useEffect } from 'react';
 
- const Home = ({posts}) => {
-  let idLink = "/" + posts._id;
-  posts.map(post => console.log(post))
+ const Home = ({prePosts}) => {
+  const[posts, setPosts] = useState(prePosts);
+  const[sorting, setSorting] = useState("default")
+  //FLITER
+  const[filtering, setFiltering] = useState("default")
+  //let idLink = "/" + posts._id;
+  useEffect(async() => {
+   console.log("changed")
+  },[sorting, filtering]);
+  const orderPost = (e) => {
+    const val = e.target.value;
+    if(val == "default"){
+      setSorting("default")
+    }
+    else if(val == "High likes"){
+      console.log("high")
+      setSorting("high")
+    }
+    else if(val == "Low likes"){
+      console.log("low")
+      setSorting("low")
+    }
+  }
+  const filterPost = (e) => {
+    const val = e.target.value;
+    if(val == "default"){
+      setFiltering("default")
+    }
+    else{
+      console.log("something")
+      setFiltering(val)
+    }
+  }
+  //posts.map(post => console.log(post))
   return (
     <div>
-      {posts.map((post) => {
+      <div>
+      <label htmlFor="ordering">Sort by:</label>
+        <select name="ordering" id="ordering" onClick={orderPost}>
+          <option value="default">Default</option>
+          <option value="High likes">High Likes</option>
+          <option value="Low likes">Low likes</option>
+        </select>
+      <label htmlFor="filtering">Filter by:</label>
+        <select name="filtering" id="filtering" onClick={filterPost}>
+          <option value="default">Default</option>
+          <option value="events">Events</option>
+          <option value="activites">Activites</option>
+          <option value="questions">Questions</option>
+          <option value="meta">Meta</option>
+          <option value="rant">Rant</option>
+        </select>
+      </div>
+      {posts.
+      sort((a,b) => {
+        if(sorting == "low"){
+          return a.likes - b.likes
+        }
+        else if(sorting == "high"){
+          return b.likes - a.likes
+        }
+        else{
+          return a.title.localeCompare(b.title);
+        }
+      })
+      .filter(post => {
+        if(filtering == "default"){
+          return true
+        }
+        else{
+          return post.tags.includes(filtering)
+        }
+      })
+      .map((post) => {
         return <HomeItem key={post._id} post={post} />;
       })}
     </div>
@@ -21,7 +90,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: data,
+      prePosts: data,
     },
   };
 }
