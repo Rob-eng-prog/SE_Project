c01a246a4d9729b94b4c02fbd18d4a4b0abceea6
Kervens Jasmin <jakejasminjk@gmail.com>
2021-04-29 14:52:11 -0400

    addeda style to Searchuser

--- pages/searchUser.js
+++ pages/searchUser.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import fetch from 'isomorphic-unfetch';
 import { useState, useEffect } from 'react';
+import HomeItem from '../components/HomeItem'
 
 const SearchUser = () => {
     const [qname, Setqname] = useState("");
@@ -35,13 +36,13 @@ const SearchUser = () => {
                     <h1 className="text-center text-2xl font-bold mb-6">Find a User</h1>
                     <label className="text-sm" htmlFor="username">Username</label><br/>
                     <input className="block px-1 mt-2 w-full text-gray-500 appearance-none border-b-2 border-gray-100 focus:border-gray-400 hover:border-gray-800 " onChange={handleChange} type="text" id="name" name="name" placeholder="Username" /><br/>
-                    <button className="m-auto w-full bg-red-800 rounded-md text-white mt-3 mb-2 h-10" type="submit">Join</button>
+                    
             </form>
             {data?
-            <div>
-                <h1>Info for {data.name}</h1>
-                <h1>{data.email}</h1>
-                <h1>{data.dateCreated}</h1>
+            <div className="bg-red-500 m-2 p-2">
+                <h1 className="text-2xl">Info for {data.name}</h1>
+                <h1 className="text-xl">{data.email}</h1>
+                <h1 className="text-lg">{data.dateCreated}</h1>
             </div>
             :
             <h1>Look for someone</h1>
